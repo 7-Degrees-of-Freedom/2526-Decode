28c5c0040e5f9a9adfa951bc5c9bb305cf00a39b
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-10-20 19:36:09 -0500

    Init traj calculation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/shooter/Shooter.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.subsystems.shooter;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/calculateShooterTraj.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+
+public class calculateShooterTraj {
+
+Rotation2d angle = new Rotation2d(45);
+double yI = 12.5;
+//38.75-64 ?
+double yF = 50;
+}
