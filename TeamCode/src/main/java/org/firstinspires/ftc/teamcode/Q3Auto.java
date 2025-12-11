a040b77ab469b94227a34db09fabe1ae0d8fdf02
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-12-11 08:27:16 -0600

    move q3tele to teamvcode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Q3Auto.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.Constants.IntakeSpeed;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.subsystems.drivetrain.DriveClass;
+
+@Autonomous
+public class Q3Auto extends LinearOpMode {
+
+    ElapsedTime timer = new ElapsedTime();
+    DcMotor shoot;
+    DcMotor shoot2;
+    DcMotor intake;
+
+    @Override
+    public void runOpMode() {
+        DriveClass d = new DriveClass();
+        //Intake intake = new Intake(hardwareMap);
+
+        //  Shooter shooter = new Shooter(hardwareMap);
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        shoot = hardwareMap.get(DcMotor.class,"launcher");
+        shoot2 = hardwareMap.get(DcMotor.class,"launcher2");
+        shoot.setDirection(DcMotorEx.Direction.REVERSE);
+        shoot2.setDirection(DcMotorEx.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        d.init(hardwareMap);
+
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            timer.startTime();
+            shoot.setPower(1);
+            shoot2.setPower(1);
+            if(timer.time() <= 0.2) {
+                intake.setPower(-0.05);
+            }
+            d.Timely(-1000,-1000,-1000,-1000,0.5);
+            if(timer.time() >= 10) intake.setPower(IntakeSpeed);
+        }}
+}
